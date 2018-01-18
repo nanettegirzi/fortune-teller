12e0a9153736e9c22bebc21897d337b1633f45e8
Faiza Husain and Nanette Girzi <faiza@>
2018-01-18 11:36:29 -0800

    Add logic to the JS page and made some changes in the HTMl

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,40 @@
 //BACK - END LOGIC
+function getFortune (luckyEventsFortune, unluckyEventsFortune) {
+  if ((luckyEventsFortune === "found money") || (luckyEventsFortune === "leaf") && (unluckyEventsFortune === "mirror") || (unluckyEventsFortune === "ladder") || (unluckyEventsFortune === "lost money")){
+    $("#mis-fortune").show();
 
+  } else if ((luckyEventsFortune === "rainbow") || (luckyEventsFortune === "friend") || (luckyEventsFortune === "found money") && (unluckyEventsFortune === "ghost") || (unluckyEventsFortune === "cat")){
+    $("#fortune").show();
 
+  } else {
+      alert("Please select some check boxes to see your fortune.");
+  }
+};
+
+var luckyEventsArray = [];
+var unluckyEventsArray = [];
+
+var luckyEventsFortune;
+var unluckyEventsFortune;
 
 //FRONT - END LOGIC
 $(document).ready(function(){
   $("form#fortune-teller").submit(function(event){
     event.preventDefault();
-    $("#lucky-responses").show();
     $("input:checkbox[name=lucky-events]:checked").each(function(){
-      var luckyEventsFortune = $(this).val();
-      $('#lucky-responses').append(luckyEventsFortune + "<br>");
+      luckyEventsFortune = $(this).val();
+      console.log(luckyEventsFortune);
+      luckyEventsArray.push(luckyEventsFortune);
+      // $('#fortune').show();
     });
-    $("#unlucky-responses").show();
+
     $("input:checkbox[name=unlucky-events]:checked").each(function(){
-      var unluckyEventsFortune = $(this).val();
-      $('#unlucky-responses').append(unluckyEventsFortune + "<br>");
+      unluckyEventsFortune = $(this).val();
+      console.log(unluckyEventsFortune);
+      unluckyEventsArray.push(unluckyEventsFortune);
+      // $('#mis-fortune').show();
     });
+    getFortune(luckyEventsFortune, unluckyEventsFortune);
     $('#fortune-teller').hide();
   });
 });
